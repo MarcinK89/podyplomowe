e37edcbf07013cca8a8d4b20beb95deeb545f24a
MarcinK89 <marcin.klak@o2.pl>
2022-10-09 14:27:34 +0200

    Initial commit

---
+++ src/podyplomowe.java
@@ -0,0 +1,16 @@
+public class podyplomowe {
+    public static void main(String[] args) {
+        String uczelnia;
+        String nazwakursu;
+        int iloscStudentow;
+
+        uczelnia = "WSZIB";
+        nazwakursu = "Java WEB Developer";
+        iloscStudentow = 20;
+
+        System.out.println("Na uczelni " + uczelnia + " na studiach podyplomowych " + nazwakursu + " uczestniczy " + iloscStudentow + " studentów. ");
+
+        
+
+    }
+}
